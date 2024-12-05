4827f5daa471d8af7bce5539ed50b7867d8c33fc
Michael Thaler <michael.thaler@dlr.de>
2024-12-05 13:28:43 +0100

    Updated list.go.

--- list/list.go
+++ list/list.go
@@ -1,10 +1,26 @@
 package list
 
-type Node struct {
-	data int
-	next *Node
+type Node[T any] struct {
+	data T
+	next *Node[T]
 }
 
-type LinkedList struct {
-	head *Node
+type LinkedList[T any] struct {
+	head *Node[T]
+}
+
+func (list *LinkedList[T]) insertAtBack(data T) {
+	newNode := &Node[T]{data: data, next: nil}
+
+	if list.head == nil {
+		list.head = newNode
+		return
+	}
+
+	current := list.head
+	for current.next != nil {
+		current = current.next
+	}
+
+	current.next = newNode
 }
