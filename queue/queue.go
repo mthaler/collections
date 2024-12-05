c4de5d0a9549f35420199cb4e903afae88e9d346
Michael Thaler <michael.thaler@dlr.de>
2024-12-05 12:17:59 +0100

    Updated queue.go.

--- queue/queue.go
+++ queue/queue.go
@@ -1,4 +1,17 @@
 
-struct {
-    items int[]
+type Queue struct {
+	items []int
+}
+
+func (q *Queue) Enqueue(item int) {
+	q.items = append(q.items, item)
+}
+
+func (q *Queue) Dequeue() int {
+	if len(q.items) < 1 {
+		return -1 // returns -1 if the queue is empty
+	}
+	item := q.items[0]
+	q.items = q.items[1:]
+	return item
 }
