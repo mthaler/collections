6ac3860a05661f51b8793173ff7cbe6c6adac735
Michael Thaler <michael.thaler@dlr.de>
2024-12-06 07:50:56 +0100

    Added queue_test.go.

---
+++ queue/queue_test.go
@@ -0,0 +1,14 @@
+package queue
+
+import (
+	"testing"
+)
+
+func TestQueue(t *testing.T) {
+	var q Queue[int] = Queue[int]{}
+	q.Enqueue(1)
+	var result = q.Dequeue()
+	if result != 1 {
+		t.Errorf("q.Dequeue() = %d, expeczed %d", result, 1)
+	}
+}
