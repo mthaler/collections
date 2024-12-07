23d2e5d49ff25c37d9440613ce634661a049edfd
Michael Thaler <michael.thaler@dlr.de>
2024-12-07 08:48:37 +0100

    Updated list.go again.

--- list/list.go
+++ list/list.go
@@ -1,5 +1,7 @@
 package list
 
+import "fmt"
+
 type Node[T any] struct {
 	data T
 	next *Node[T]
@@ -23,4 +25,73 @@ func (list *LinkedList[T]) Append(data T) {
 	}
 
 	current.next = newNode
+}
+
+func (list *LinkedList[T]) Prepend(data T) {
+	if list.head == nil {
+		newNode := &Node[T]{data: data, next: nil}
+		list.head = newNode
+		return
+	}
+
+	newNode := &Node[T]{data: data, next: list.head}
+	list.head = newNode
+}
+
+func (list *LinkedList[T]) DeleteFront() {
+	if list.head != nil {
+		list.head = list.head.next
+		fmt.Printf("Head node of linked list has been deleted\n")
+		return
+	}
+}
+
+func (list *LinkedList[T]) DeleteLast() {
+	if list.head == nil {
+		fmt.Printf("Linked list is empty\n")
+	}
+
+	if list.head.next == nil {
+		list.head = nil
+		fmt.Printf("Last node of linked list has been deleted\n")
+		return
+	}
+
+	var current *Node[T] = list.head
+	for current.next.next != nil {
+		current = current.next
+	}
+	current.next = nil
+
+	fmt.Printf("Last node of linked list has been deleted")
+}
+
+func (list *LinkedList[T]) CountNodes() int {
+	count := 0
+	current := list.head
+	for current != nil {
+		current = current.next
+		count++
+	}
+	return count
+}
+
+func (list *LinkedList[T]) FindNodeAt(index int) *Node[T] {
+	var count int = 0
+	var current *Node[T] = list.head
+
+	for current != nil {
+		count++
+		current = current.next
+	}
+
+	if index <= 0 || index > count {
+		return nil
+	}
+
+	current = list.head
+	for count = 1; count < index; count++ {
+		current = current.next
+	}
+	return current
 }
