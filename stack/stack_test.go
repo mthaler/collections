f895c2c0ef08abf9eccd6c75ec112254f2ef6c1e
Michael Thaler <michael.thaler@dlr.de>
2024-12-11 08:16:44 +0100

    Updated stack_test.go.

--- stack/stack_test.go
+++ stack/stack_test.go
@@ -1,6 +1,9 @@
 package stack
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestIsEmpty(t *testing.T) {
 	var s Stack[int]
@@ -10,5 +13,17 @@ func TestIsEmpty(t *testing.T) {
 	s.Push(1)
 	if s.IsEmpy() {
 		t.Errorf("Stack should be empty")
+	}
+}
+
+func TestToSlice(t *testing.T) {
+	var s Stack[int]
+	s.Push(1)
+	s.Push(2)
+	s.Push(3)
+	expected := []int{1, 2, 3}
+	result := s.ToSlice()
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("s = %v, expected %v", s, expected)
 	}
 }
