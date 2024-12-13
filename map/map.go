84c878c487364f7e8706a54c4b3480f0e4db54ea
Michael Thaler <michael.thaler@dlr.de>
2024-12-13 16:36:09 +0100

    Updated map.go.

--- map/map.go
+++ map/map.go
@@ -1,11 +1,30 @@
 package mymap
 
-type HashST struct {
+import "errors"
+
+type Node struct {
+	Key   any
+	Value any
+	next  *Node
+}
+
+type HashST[K comparable, V any] struct {
 	n  int    // number of key-value pairs
 	m  int    // number of chains
 	st []Node // array of linked-list symbol tables
 }
 
-func (st *HashST) Size() int {
+func (st *HashST[K, V]) Size() int {
 	return st.n
+}
+
+func (st *HashST[K, V]) IsEmpty() bool {
+	return st.Size() == 0
+}
+
+func (st *HashST[K, V]) Contains(key K) (bool, error) {
+	if key == nil {
+		return false, errors.New("argument to contains() is nil")
+	}
+	return get(key) != nil
 }
