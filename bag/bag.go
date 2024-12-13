4fcb9a8b810e24663dc70b2919f7a31e9f80078e
Michael Thaler <michael.thaler@dlr.de>
2024-12-13 11:07:45 +0100

    Added bag.go.

---
+++ bag/bag.go
@@ -0,0 +1,43 @@
+package bag
+
+type Bag[T any] struct {
+	first *Node[T] // beginning of bag
+	n     int      // number of elements in bag
+}
+
+type Node[T any] struct {
+	item T
+	next *Node[T]
+}
+
+/**
+ * Returns true if this bag is empty.
+ *
+ * @return {@code true} if this bag is empty;
+ *         {@code false} otherwise
+ */
+func (b *Bag[T]) IsEmpty() bool {
+	return b.first == nil
+}
+
+/**
+ * Returns the number of items in this bag.
+ *
+ * @return the number of items in this bag
+ */
+func (b *Bag[T]) Size() int {
+	return b.n
+}
+
+/**
+ * Adds the item to this bag.
+ *
+ * @param  item the item to add to this bag
+ */
+func (b *Bag[T]) Add(t T) {
+	oldFirst := b.first
+	var first Node[T]
+	first.item = t
+	first.next = oldFirst
+	b.n++
+}
