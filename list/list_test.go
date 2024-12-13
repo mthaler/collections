3e4a191ffcd9c54bec9890956a65f2327beaae00
Michael Thaler <michael.thaler@dlr.de>
2024-12-13 12:14:33 +0100

    Updated list_test.go again.

--- list/list_test.go
+++ list/list_test.go
@@ -16,12 +16,24 @@ func TestIsEmpty(t *testing.T) {
 	}
 }
 
-func TestIPrepend(t *testing.T) {
+func TestPrepend(t *testing.T) {
 	var l LinkedList[int]
 	l.Prepend(1)
 	l.Prepend(2)
 	l.Prepend(3)
 	expected := []int{3, 2, 1}
+	result := l.ToSlice()
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("l = %v, expected %v", result, expected)
+	}
+}
+
+func TestAppend(t *testing.T) {
+	var l LinkedList[int]
+	l.Append(1)
+	l.Append(2)
+	l.Append(3)
+	expected := []int{1, 2, 3}
 	result := l.ToSlice()
 	if !reflect.DeepEqual(result, expected) {
 		t.Errorf("l = %v, expected %v", result, expected)
