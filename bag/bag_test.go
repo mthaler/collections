bcd495fbd396a7ee82d818d0a2e922d10286d8b3
Michael Thaler <michael.thaler@dlr.de>
2024-12-13 11:26:12 +0100

    Updated bag_test.go.

--- bag/bag_test.go
+++ bag/bag_test.go
@@ -1,6 +1,10 @@
 package bag
 
-import "testing"
+import (
+	"reflect"
+	"sort"
+	"testing"
+)
 
 func TestQueue(t *testing.T) {
 	var b Bag[int]
@@ -20,5 +24,18 @@ func TestSize(t *testing.T) {
 	b.Add(3)
 	if b.Size() != 3 {
 		t.Errorf("s.Size() = %d, expected %d", b.Size(), 3)
+	}
+}
+
+func TestToSlice(t *testing.T) {
+	var b Bag[int]
+	b.Add(1)
+	b.Add(2)
+	b.Add(3)
+	expected := []int{1, 2, 3}
+	result := b.ToSlice()
+	sort.Ints(result)
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("s = %v, expected %v", result, expected)
 	}
 }
