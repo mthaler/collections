8915030caf88cf89273d9d4cbdf117e14ac4f7a4
Michael Thaler <michael.thaler@dlr.de>
2024-12-13 11:18:35 +0100

    Updared bag_test.go.

--- bag/bag_test.go
+++ bag/bag_test.go
@@ -12,3 +12,13 @@ func TestQueue(t *testing.T) {
 		t.Errorf("Bag should not be empty")
 	}
 }
+
+func TestSize(t *testing.T) {
+	var b Bag[int]
+	b.Add(1)
+	b.Add(2)
+	b.Add(3)
+	if b.Size() != 3 {
+		t.Errorf("s.Size() = %d, expected %d", b.Size(), 3)
+	}
+}
