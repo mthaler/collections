fd3d057bdf3339e83768e8e77f66e8e2dee761dd
Michael Thaler <michael.thaler@dlr.de>
2024-12-23 14:30:32 +0100

    Updated go.mod and map.go.

--- map/map.go
+++ map/map.go
@@ -24,7 +24,7 @@ func (st *HashST[K, V]) resize(chains int) {
 	temp := NewWithChains{K, V}(chains)
 	for i := 0; i < st.m; i++ {
 		for x := st.st[i]; x != nil; x = x.next {
-			temp.Put((Key), x.Key, V(x.Value))
+			temp.Put(x.Key.(K), x.Value.(V))
 		}
 	}
 
@@ -34,7 +34,7 @@ func (st *HashST[K, V]) resize(chains int) {
 }
 
 func (st *HashST[K, V]) hash(key K) int {
-	hash, err := hashstructure.Hash(key, hashstructure.FormatV2, nil)
+	hash := hashstructure.Hash(key, hashstructure.FormatV2, nil)
 	return (hash & 0x7fffffff) % st.m
 }
 
@@ -54,7 +54,7 @@ func (st *HashST[K, V]) Contains(key K) bool {
 func (st *HashST[K, V]) Get(key K) (V, error) {
 	i := st.hash(key)
 	for x := st.st[i]; x != nil; x = x.next {
-		if key.equals(x.key) {
+		if key == x.Key {
 			return V(x.Value), nil
 
 		}
@@ -70,33 +70,33 @@ func (st *HashST[K, V]) Put(key K, value V) {
 
 	i := hash(key)
 	NewWithChains
-	for x := st[i]; x != null; x = x.next {
-		if key.equals(x.key) {
-			x.Value = val
+	for x := st.st[i]; x != nil; x = x.next {
+		if key == x.Key {
+			x.Value = value
 			return
 		}
 	}
 	st.n++
-	st[i] = Node(key, val, st[i])
+	st.st[i] = Node(key, value, st.st[i])
 }
 
 func (st *HashST[K, V]) Remove(key K) {
-	i := hash(key)
-	st.st[i] = remove(st.st[i], key)
+	i := st.hash(key)
+	st.st[i] = st.remove(*st.st[i], key)
 
 	// halve table size if average length of list <= 2
-	if m > INIT_CAPACITY && n <= 2*m {
-		st.resize(m / 2)
+	if st.m > INIT_CAPACITY && st.n <= 2*st.m {
+		st.resize(st.m / 2)
 	}
 }
 
 func (st *HashST[K, V]) remove(x Node, key K) *Node {
-	if key.equals(x.Key) {
+	if key == x.Key {
 		st.n--
 		return x.next
-		x.next = st.remove(x.next, key)
-		return x
 	}
+	x.next = st.remove(*x.next, key)
+	return &x
 }
 
 func New[K comparable, V any]() HashST[K, V] {
