a9aa24a93f6c8fd69d5f8e871bf53121ae493a9e
Michael Thaler <michael.thaler@dlr.de>
2024-12-25 12:10:54 +0100

    Updated util_test.go.

--- util/util_test.go
+++ util/util_test.go
@@ -1,1 +1,15 @@
 package util
+
+import (
+	mylist "collections/list"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestToSlice(t *testing.T) {
+	l := mylist.NewList[int]()
+	assert.True(t, h.IsEmpty(), "Map should be empty")
+	h.Put(1, 1234)
+	assert.False(t, h.IsEmpty(), "Map should not be empty")
+}
