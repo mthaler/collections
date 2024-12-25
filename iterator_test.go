c3c178d1f943f4e58bbe6a1d2362099f9f52076c
Michael Thaler <michael.thaler@dlr.de>
2024-12-25 12:24:26 +0100

    Updated iterator_test.go.

--- iterator_test.go
+++ iterator_test.go
@@ -11,4 +11,7 @@ func TestHasNext(t *testing.T) {
 	l := mylist.NewList[int]()
 	it := l.CreateIterator()
 	assert.False(t, it.HasNext(), "Iterator.HasNext should be false")
+	l = mylist.NewList(1, 2, 3)
+	it = l.CreateIterator()
+	assert.True(t, it.HasNext(), "Iterator.HasNext should be true")
 }
