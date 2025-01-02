d49882b0b9003e0ef730685e569a5621540774bb
Michael Thaler <michael.thaler@dlr.de>
2025-01-02 15:01:33 +0100

    Updated bag_test.go.

--- bag/bag_test.go
+++ bag/bag_test.go
@@ -4,17 +4,15 @@ import (
 	"reflect"
 	"sort"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestQueue(t *testing.T) {
+func TestIsEmpty(t *testing.T) {
 	var b Bag[int]
-	if !b.IsEmpty() {
-		t.Errorf("Bag should be empty")
-	}
+	assert.True(t, b.IsEmpty())
 	b.Add(1)
-	if b.IsEmpty() {
-		t.Errorf("Bag should not be empty")
-	}
+	assert.False(t, b.IsEmpty())
 }
 
 func TestSize(t *testing.T) {
@@ -22,15 +20,14 @@ func TestSize(t *testing.T) {
 	b.Add(1)
 	b.Add(2)
 	b.Add(3)
-	if b.Size() != 3 {
-		t.Errorf("s.Size() = %d, expected %d", b.Size(), 3)
-	}
+	assert.Equal(t, 3, b.Size())
 }
 
 func TestToSlice(t *testing.T) {
 	var b Bag[int]
 	b.Add(1)
 	b.Add(2)
+
 	b.Add(3)
 	expected := []int{1, 2, 3}
 	result := b.ToSlice()
