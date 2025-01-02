a6a3b837863a0733d3e662411170121a026aee18
Michael Thaler <michael.thaler@dlr.de>
2025-01-02 18:59:49 +0100

    Updated hashset.go.

--- set/hashset.go
+++ set/hashset.go
@@ -18,6 +18,11 @@ func (s *HashSet[K, V]) Contains(key K) bool {
 	return s.m.Contains(key)
 }
 
+func (s *HashSet[K, V]) Put(key K) {
+	var value V
+	s.m.Put(key, value)
+}
+
 func New[K comparable, V any]() HashSet[K, V] {
 	st := mymap.New[K, V]()
 	return HashSet[K, V]{m: st}
