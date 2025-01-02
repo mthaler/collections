324a37a79c7b0371db7dee22bbb10a8cd68e983a
Michael Thaler <michael.thaler@dlr.de>
2025-01-02 16:05:32 +0100

    Updated hashmap_test.go.

--- map/hashmap_test.go
+++ map/hashmap_test.go
@@ -8,7 +8,15 @@ import (
 
 func TestIsEmpty(t *testing.T) {
 	h := New[int, int]()
-	assert.True(t, h.IsEmpty(), "Map should be empty")
+	assert.True(t, h.IsEmpty())
 	h.Put(1, 1234)
-	assert.False(t, h.IsEmpty(), "Map should not be empty")
+	assert.False(t, h.IsEmpty())
+}
+
+func TestSize(t *testing.T) {
+	h := New[int, int]()
+	h.Put(1, 1)
+	h.Put(2, 2)
+	h.Put(3, 3)
+	assert.Equal(t, 3, h.Size())
 }
