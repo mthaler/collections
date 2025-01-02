5f4d4d8b61fb6efbd1ea02488f21ebb4d5b74c22
Michael Thaler <michael.thaler@dlr.de>
2025-01-02 17:02:58 +0100

    Updated hashset_test.go.

--- set/hashset_test.go
+++ set/hashset_test.go
@@ -12,3 +12,11 @@ func TestIsEmpty(t *testing.T) {
 	h.m.Put(1, 1234)
 	assert.False(t, h.IsEmpty(), "Map should not be empty")
 }
+
+func TestSize(t *testing.T) {
+	h := New[int, int]()
+	h.m.Put(1, 1)
+	h.m.Put(2, 2)
+	h.m.Put(3, 3)
+	assert.Equal(t, 3, h.Size())
+}
