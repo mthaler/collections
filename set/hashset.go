e806f457d8157b0d6b76d929fe0924d5fb668d35
Michael Thaler <michael.thaler@dlr.de>
2025-01-03 07:10:58 +0100

    Updated hashset.go.

--- set/hashset.go
+++ set/hashset.go
@@ -2,28 +2,27 @@ package set
 
 import mymap "collections/map"
 
-type HashSet[K comparable, V any] struct {
-	m mymap.HashST[K, V]
+type HashSet[K comparable] struct {
+	m mymap.HashST[K, any]
 }
 
-func (s *HashSet[K, V]) Size() int {
+func (s *HashSet[K]) Size() int {
 	return s.m.Size()
 }
 
-func (s *HashSet[K, V]) IsEmpty() bool {
+func (s *HashSet[K]) IsEmpty() bool {
 	return s.m.IsEmpty()
 }
 
-func (s *HashSet[K, V]) Contains(key K) bool {
+func (s *HashSet[K]) Contains(key K) bool {
 	return s.m.Contains(key)
 }
 
-func (s *HashSet[K, V]) Put(key K) {
-	var value V
-	s.m.Put(key, value)
+func (s *HashSet[K]) Put(key K) {
+	s.m.Put(key, interface{}{})
 }
 
-func New[K comparable, V any]() HashSet[K, V] {
-	st := mymap.New[K, V]()
-	return HashSet[K, V]{m: st}
+func New[K comparable]() HashSet[K] {
+	st := mymap.New[K, any]()
+	return HashSet[K]{m: st}
 }
