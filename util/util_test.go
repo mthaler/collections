c716d3b3ea529636f9834c4e0e851befb8c1e7f7
Michael Thaler <michael.thaler@dlr.de>
2025-01-03 09:10:44 +0100

    Deleted iterator_test.go.

--- util/util_test.go
+++ util/util_test.go
@@ -8,9 +8,9 @@ import (
 )
 
 func TestToSlice(t *testing.T) {
-	l := mylist.NewList[int]()
+	l := mylist.New[int]()
 	assert.True(t, l.IsEmpty())
-	l = mylist.NewList[int](1, 2, 3)
+	l = mylist.New[int](1, 2, 3)
 	result := ToSclice[int](l)
 	expected := []int{1, 2, 3}
 	assert.Equal(t, expected, result)
