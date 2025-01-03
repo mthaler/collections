69a9dec40d8c285d2a57238f4a94dfa2d70124be
Michael Thaler <michael.thaler@dlr.de>
2025-01-03 08:01:46 +0100

    Updated hashset_test.go.

--- set/hashset_test.go
+++ set/hashset_test.go
@@ -7,25 +7,32 @@ import (
 )
 
 func TestIsEmpty(t *testing.T) {
-	h := New[int]()
-	assert.True(t, h.IsEmpty(), "Map should be empty")
-	h.m.Put(1, 1234)
-	assert.False(t, h.IsEmpty(), "Map should not be empty")
+	s := New[int]()
+	assert.True(t, s.IsEmpty())
+	s.m.Put(1, 1234)
+	assert.False(t, s.IsEmpty())
 }
 
 func TestSize(t *testing.T) {
-	h := New[int]()
-	h.m.Put(1, 1)
-	h.m.Put(2, 2)
-	h.m.Put(3, 3)
-	assert.Equal(t, 3, h.Size())
+	s := New[int]()
+	s.m.Put(1, 1)
+	s.m.Put(2, 2)
+	s.m.Put(3, 3)
+	assert.Equal(t, 3, s.Size())
 }
 
 func TestContains(t *testing.T) {
-	h := New[int]()
-	h.Put(1)
-	h.Put(2)
-	h.Put(3)
-	assert.True(t, h.Contains(3))
-	assert.False(t, h.Contains(4))
+	s := New[int]()
+	s.Put(1)
+	s.Put(2)
+	s.Put(3)
+	assert.True(t, s.Contains(3))
+	assert.False(t, s.Contains(4))
+}
+
+func TestPut(t *testing.T) {
+	s := New[int]()
+	assert.False(t, s.Contains(1))
+	s.Put(1)
+	assert.True(t, s.Contains(1))
 }
