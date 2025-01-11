48439de2417e5650de87c29bc278c396a84edff5
Michael Thaler <michael.thaler@dlr.de>
2025-01-11 10:16:35 +0100

    Updated hashmap_test.go.

--- map/hashmap_test.go
+++ map/hashmap_test.go
@@ -44,11 +44,13 @@ func TestPut(t *testing.T) {
 	assert.Equal(t, 1234, v)
 }
 
-/*func TestRemove(t *testing.T) {
+func TestRemove(t *testing.T) {
 	h := New[int, int]()
 	h.Put(1, 1)
 	h.Put(2, 2)
 	h.Put(3, 3)
 	h.Remove(1)
-	assert.Equal(t, []int{1, 3}, util.ToSclice[h]())
-}*/
+	assert.False(t, h.Contains(1))
+	assert.True(t, h.Contains(2))
+	assert.True(t, h.Contains(3))
+}
